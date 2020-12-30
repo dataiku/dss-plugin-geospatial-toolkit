ee84a53cb1f40a869616647e3501dbcbb4de2377
Thibault Desfontaines <desfontainesthibault@gmail.com>
2020-12-30 15:31:19 +0100

    Minimal workaround for the custom UI, temporary and for testing purpose only leftBarParamsTemplate.html: Set up a toy example for the UI leftBarParamsModule.js

--- js/leftBarParamsModule.js
+++ js/leftBarParamsModule.js
@@ -5,4 +5,10 @@ app.controller('MyCustomFormController', ['$scope', function($scope) {
     $scope.config.value = 75;
     $scope.config.min = 10;
     $scope.config.max = 90;
+    $scope.config.color = {
+        name: 'blue'
+    };
+    $scope.config.maptile = {
+        name: 'mapbox/v11'
+    };
 }]);
