f847d495e1a054ee307c1774a8ccc01761a3ea5c
Thibault Desfontaines <desfontainesthibault@gmail.com>
2021-02-16 18:33:49 +0100

    [ch61233] Set sampling to the same configuration as in DSS Since we cannot get the sampling from DSS, we will give the default DSS configuration by default.

--- python-lib/dku_data_processing/format.py
+++ python-lib/dku_data_processing/format.py
@@ -69,7 +69,8 @@ def fetch_geo_data(dss_dataset: Dataset, geopoint, detail, tooltips, filters):
         for col_ in filters:
             column_to_retrieves.add(col_['column'])
 
-    df = dss_dataset.get_dataframe(limit=1000, columns=column_to_retrieves)
+    # Sampling should be the same as DSS by default
+    df = dss_dataset.get_dataframe(sampling='head', limit=10000, columns=column_to_retrieves)
 
     if (filters is not None) and (filters != []):
         df = filter_dataframe(df, filters)
