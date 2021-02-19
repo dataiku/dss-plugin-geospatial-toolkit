36a75fd0eaf2f8d02ef1058c9d5a10126cd31c93
Thibault Desfontaines <desfontainesthibault@gmail.com>
2021-02-19 14:31:46 +0100

    Change the logic behind the generated name on a Dataframe

--- python-lib/utils/df_column_naming.py
+++ python-lib/utils/df_column_naming.py
@@ -1,16 +1,9 @@
 import logging
-import random
-import string
 
 from collections import defaultdict
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format='%(asctime)s Custom Chart Geospatial Density  | %(levelname)s - %(message)s')
-
-
-def get_random_key():
-    len_key = 10
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=len_key))
 
 
 def get_new_column_name(df, prefix):
@@ -27,8 +20,7 @@ def get_new_column_name(df, prefix):
         name_is_taken[column] = True
     if not name_is_taken[prefix]:
         return prefix
-    for iteration in range(2*len(columns)):
-        generated_name = prefix + "_" + get_random_key()
+    for suffix in range(len(columns)+1):
+        generated_name = prefix + "_" + str(suffix)
         if not name_is_taken[generated_name]:
             return generated_name
-    raise ValueError("Impossible to generate a name starting by {} from the dataset.".format(prefix))
