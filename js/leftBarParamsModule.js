5f8c01a59d91c012b74473c45d380c8d9710e92b
Thibault Desfontaines <desfontainesthibault@gmail.com>
2021-02-19 16:18:39 +0100

    Removed unused controller interactions. topBarParamsModule.js: Removed since controller is not called. leftBarParamsModule.js: Simplify logic since default value initialisation are not mandatory topBarParamsTemplate.html: Removed unused template

--- js/leftBarParamsModule.js
+++ js/leftBarParamsModule.js
@@ -1,14 +1,3 @@
 var app = angular.module('geospatial.density', []);
 
-const DEFAULT_INTENSITY = 50;
-const DEFAULT_RADIUS = 50;
-const DEFAULT_COLOR_SCALE = null;
-
-app.controller('MyCustomFormController', ['$scope', function($scope) {
-    console.log("Calling the controller");
-    // Variables initialisation
-    $scope.config.intensity = DEFAULT_INTENSITY;
-    $scope.config.radius = DEFAULT_RADIUS;
-    $scope.config.color = DEFAULT_COLOR;
-    $scope.config.maptiles = backgrounds;
-}]);
+app.controller('MyCustomFormController', ['$scope', function($scope) {}]);
